c020bdc8ea200fc9cd61a9fd3a4c77f16e3dc142
Sarthak Jain <sarthak.7386@gmail.com>
2014-02-01 11:12:40 +0530

    TEXT

---
+++ src/freetts/FreeTTS.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package freetts;
+
+/**
+ *
+ * @author Dell
+ */
+public class FreeTTS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new FormTTS().setVisible(true);
+    }
+}
